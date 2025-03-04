5ddfc3756a110b7a1c115533d59c8f4830436bdc
BA-SS-IL <basilvarghese4566@gmail.com>
2025-03-04 16:18:32 +0530

    view engine setup

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const env = require('dotenv').config();
-const db = require('./config/db')
-db()
+const db = require('./config/db');
+const userRouter = require('./routes/userRouter')
+
+db();
+
+app.use(express.json());//middleware for json parson
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/adm')]);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/',userRouter)
 
 
 app.listen(process.env.PORT,()=>{
